d484f214d2a3430706874af3d91957ead4b2e00b
Kaushal M <kshlmster@gmail.com>
2010-07-29 11:31:17 +0530

    Added definition for Document Extractor. It is a class which extracts the text from the Text Document.

--- documentextractor.cpp
+++ documentextractor.cpp
@@ -1,5 +1,78 @@
 #include "documentextractor.h"
 
+#include <kparts/part.h>
+#include <kparts/componentfactory.h>
+#include <kmimetype.h>
+#include <KoView.h>
+#include <KWView.h>
+#include <KoTextEditor.h>
+#include <kurl.h>
+#include <KoToolProxy.h>
+#include <KWCanvas.h>
+
+#include <QtXml>
+#include <QtCore>
+#include <QTextDocument>
+
 documentExtractor::documentExtractor()
 {
+}
+
+QString documentExtractor::getBody(QString &filePath)
+{
+    KUrl url;
+    url.setPath(filePath);
+    QString mimetype = KMimeType::findByUrl()->name();
+    KoDocument *kdoc = KParts::ComponentFactory::createPartInstanceFromQuery<KoDocument>(mimetype, QString::null);
+    kdoc->openUrl(url);
+    KoView *koview = kdoc->createView();
+    KWView *kwview = qobject_cast<KWView *>(koview);
+    KoTextEditor *editor = qobject_cast<KoTextEditor *>(kwview->kwcanvas()->toolProxy()->selection());
+    QTextDocument *tdoc = editor->document();
+    QString data = tdoc->toHtml();
+
+    QDomDocument ddoc;
+    ddoc.setContent(data.toUtf8());
+    QDomElement bodyNode = ddoc.documentElement().firstChildElement("body");
+    removeColor(bodyNode);
+
+    QString body;
+    QTextStream ts(body);
+    bodyNode.save(ts, 0);
+
+    removeTags(body);
+
+    return body;
+}
+
+void documentExtractor::removeColor(QDomElement &node)
+{
+    if(node.hasChildNodes()) {
+        QDomNodeList children = node.childNodes();
+        for(int i = 0; i < children.length(); i++) {
+            QDomNode x = children.at(i);
+            if(x.isElement()) {
+                QDomElement e = x.toElement();
+                removeColor(e);
+            }
+        }
+    }
+    if(node.hasAttribute("style")) {
+        QString style = node.attribute("style");
+        node.removeAttribute("style");
+        if(node.tagName() == "td")
+            return;
+        style = style.remove(QRegExp("color*;", Qt::CaseInsensitive, QRegExp::Wildcard));
+        style = style.remove(QRegExp("background-", Qt::CaseInsensitive, QRegExp::Wildcard));
+        node.setAttribute("style", style);
+    }
+    return;
+}
+
+void documentExtractor::removeTags(QString &body)
+{
+    body = body.right(body.size() - body.indexOf("<td>") - 4);
+    body = body.left(body.lastIndexOf("</td>"));
+    body.remove(QRegExp("<(?:span)[^>]*>", Qt::CaseInsensitive));
+    body.remove(QRegExp("</span>", Qt::CaseInsensitive));
 }
--- documentextractor.h
+++ documentextractor.h
@@ -1,10 +1,19 @@
 #ifndef DOCUMENTEXTRACTOR_H
 #define DOCUMENTEXTRACTOR_H
 
+class QString;
+class QDomElement;
+
 class documentExtractor
 {
 public:
     documentExtractor();
+
+    QString getBody(QString & filePath);
+
+private:
+    void removeColor(QDomElement & node);
+    void removeTags(QString & body);
 };
 
 #endif // DOCUMENTEXTRACTOR_H
